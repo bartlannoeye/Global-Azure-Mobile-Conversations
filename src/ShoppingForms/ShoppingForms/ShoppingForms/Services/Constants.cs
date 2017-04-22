2feca21401cb66ec0eba6f7f79871e564b0aa2e4
Bart Lannoeye <bart.lannoeye@gmail.com>
2017-04-22 18:14:41 +0200

    Added Constants file without keys (sorry)

---
+++ src/ShoppingForms/ShoppingForms/ShoppingForms/Services/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace FormsChatInterface.Services
+{
+    public class Constants
+    {
+		// key from https://dev.botframework.com
+        public const string DirectLineKey = "Get an endpoint key for your bot";
+
+        public const string MyUserId = "4FE3922";
+        public const string MyName = "Me";
+        public const string FriendName = "My Cup<T>";
+    }
+}
